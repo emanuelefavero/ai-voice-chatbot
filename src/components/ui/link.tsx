004eb888d9cb4c5a697f215af5805f77375b1f58
Emanuele Favero <emafaux89@gmail.com>
2026-02-13 00:30:29 +0100

    feat: add link component in ui folder

---
+++ src/components/ui/link.tsx
@@ -0,0 +1,31 @@
+import { cva, type VariantProps } from 'class-variance-authority'
+import NextLink from 'next/link'
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
+const linkVariants = cva(
+  'underline-offset-4 outline-none hover:underline focus-visible:underline',
+  {
+    variants: {
+      variant: {
+        default: 'text-link',
+      },
+    },
+    defaultVariants: {
+      variant: 'default',
+    },
+  },
+)
+
+function Link({
+  className,
+  variant = 'default',
+  ...props
+}: React.ComponentProps<typeof NextLink> & VariantProps<typeof linkVariants>) {
+  return (
+    <NextLink className={cn(linkVariants({ variant, className }))} {...props} />
+  )
+}
+
+export { Link, linkVariants }
