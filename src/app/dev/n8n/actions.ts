bc2398418b3a3b29272cd6d6d1227f5481e80e53
Emanuele Favero <emafaux89@gmail.com>
2026-02-16 10:59:03 +0100

    chore: clean up comments in n8n/actions.ts

--- src/app/dev/n8n/actions.ts
+++ src/app/dev/n8n/actions.ts
@@ -6,28 +6,6 @@ import { InputData, Response, ResponseData } from './types'
 
 const URL = process.env.N8N_WEBHOOK_URL
 
-/*
-Reference:
-- Form input data: email
-- Server action will validate the input and send a POST request to N8N_WEBHOOK_URL with the email in the body. The server action will also validate the response from n8n and return a success/error response to the client.
-- N8N workflow:
-  - Trigger: Webhook (POST to N8N_WEBHOOK_URL)
-  - N8N will receive the email, it will add a submittedAt timestamp, and send the data as a message with Telegram
-  - Example Response:
-    - Success: {
-  "ok": true,
-  "data": {
-    "id": 20,
-    "email": "john@example.com",
-    "submittedAt": "2026-02-16T02:01:24.226+01:00"
-  }
-
-  - Error: {
-  "ok": false,
-  "error": "Failed to submit form"
-  }
-*/
-
 export async function submitForm(
   data: InputData,
 ): Promise<Response<ResponseData>> {
